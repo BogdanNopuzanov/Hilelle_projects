3c2818fdf465b59ef93a9dc9302c9e1b6f71cdfa
bogdan_top <bogdan.iron.fist@gmail.com>
2026-03-23 20:09:07 +0200

    sec commit

--- dz3/main1.py
+++ dz3/main1.py
@@ -1,4 +1,3 @@
-
 pershe_chislo=float(input('Pershe chislo: '))
 deistvie=str(input('vvedite + - * /: '))
 druge_chislo=float(input('Druge chislo: '))
@@ -12,8 +11,8 @@ elif deistvie=='*':
 elif deistvie=='/':
     if druge_chislo==0:
         rezultat= 0
-        print('ne delim naa 0')
+        print('ne delim na 0')
     else:
         rezultat= pershe_chislo/druge_chislo
 
-print('otvet :',rezultat)
+print(pershe_chislo,deistvie,druge_chislo,rezultat)
