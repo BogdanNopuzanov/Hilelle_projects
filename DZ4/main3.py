1bbb149b8f7a8de0b97131c6d60d2284fc77e5eb
BogdanTopuzanov <79829528+BogdanNopuzanov@users.noreply.github.com>
2026-03-30 19:58:32 +0300

    Delete DZ4 directory

--- DZ4/main3.py
+++
@@ -1,8 +0,0 @@
-import random
-def main1():
-    list_1=[]
-    for i in range(random.randint(3, 10)):
-        list_1.append(random.randint(3, 10))
-    print(list_1)
-    print(list_1[0], list_1[2], list_1[-2])
-main1()
