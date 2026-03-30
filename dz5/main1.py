8ecb76337861110dbbf112c74aa571f404241fb2
bogdan_top <bogdan.iron.fist@gmail.com>
2026-03-30 19:58:05 +0300

    sec commit

--- dz5/main1.py
+++ dz5/main1.py
@@ -3,3 +3,4 @@ my_string = "I like PHP, PHP, PHP,"
 # можна вказувати кілька методів поспіль
 my_string1 = my_string.replace("PHP", "Python").replace(',', '!')
 print(my_string1) # виведе: I like Python! Python! Python!
+
